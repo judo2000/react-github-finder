4803c8e500e427c7841e7b1feb25b1c466e2a938
Stephen Moore <judo2000@comcast.net>
2022-04-29 12:45:16 -0600

    Add github stats to single user profile page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,4 +1,4 @@
-import { FaCodepen, FaStore, faUserFriends, FaUsers } from "react-icons/fa";
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { useEffect, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
 import Spinner from "../components/layout/Spinner";
@@ -37,7 +37,7 @@ function User() {
       <div className='w-full mx-auto lg:w-10/12'>
         <div className='mb-4'>
           <Link to='/' className='btn btn-ghost'>
-            Back to Search
+            Back To Search
           </Link>
         </div>
 
@@ -56,7 +56,7 @@ function User() {
 
           <div className='col-span-2'>
             <div className='mb-6'>
-              <h1 className='text-3xl car-title'>
+              <h1 className='text-3xl card-title'>
                 {name}
                 <div className='ml-2 mr-1 badge badge-success'>{type}</div>
                 {hireable && (
@@ -64,7 +64,7 @@ function User() {
                 )}
               </h1>
               <p>{bio}</p>
-              <div className='mt-4 card-action'>
+              <div className='mt-4 card-actions'>
                 <a
                   href={html_url}
                   target='_blank'
@@ -73,6 +73,83 @@ function User() {
                 >
                   Visit Github Profile
                 </a>
+              </div>
+            </div>
+
+            <div className='w-full rounded-lg shadow-md bg-base-100 stats'>
+              {location && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>Location</div>
+                  <div className='text-lg stat-value'>{location}</div>
+                </div>
+              )}
+              {blog && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>Website</div>
+                  <div className='text-lg stat-value'>
+                    <a href={html_url} target='_blank' rel='noreferrer'>
+                      {html_url}
+                    </a>
+                  </div>
+                </div>
+              )}
+              {twitter_username && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>Twitter</div>
+                  <div className='text-lg stat-value'>
+                    <a
+                      href={`https://twitter.com/${twitter_username}`}
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      {twitter_username}
+                    </a>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats'>
+          <div className='grid grid-cols-1 md:grid-cols-3'>
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaUsers className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Followers</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {followers}
+              </div>
+            </div>
+
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaUserFriends className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Following</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {following}
+              </div>
+            </div>
+
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaCodepen className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Public Repos</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {public_repos}
+              </div>
+            </div>
+
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaStore className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Public Gists</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {public_gists}
               </div>
             </div>
           </div>
