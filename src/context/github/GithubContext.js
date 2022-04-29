bcb3eb9ca1c490cbbd47000f643f956692b90719
Stephen Moore <judo2000@comcast.net>
2022-04-29 17:23:59 -0600

    Mmove searchUsers from the context to and action file

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -34,28 +34,6 @@ export const GithubProvider = ({ children }) => {
   //   });
   // };
 
-  // Get search results
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token: ${GITHUB_TOKEN}`,
-      },
-    });
-
-    const { items } = await response.json();
-
-    dispatch({
-      type: "GET_USERS",
-      payload: items,
-    });
-  };
-
   // Get single user
   const getUser = async (login) => {
     setLoading();
@@ -113,11 +91,8 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
-        searchUsers,
+        ...state,
+        dispatch,
         clearUsers,
         getUser,
         getUserRepos,
